2b2b858130c4e629def9102672f97408cf1452e2
Anthony YOU <spleen85@proton.me>
2025-01-15 15:58:32 +0100

    BruteForce ok

--- bruteforce.py
+++ bruteforce.py
@@ -1,11 +1,12 @@
 import csv
+from itertools import combinations
 
 actions = []
-max_expense = 500
+MAX_EXPENSE = 500
 
 
 def readFile():
-    with open("Liste actions.csv") as csv_file:
+    with open("data/Liste actions.csv") as csv_file:
         csv_data = csv.reader(csv_file, delimiter=",")
         for data in csv_data:
             try:
@@ -17,9 +18,40 @@ def readFile():
                 })
             except ValueError:
                 None
-#    return csv_data
+
+
+def calculate_cost(combination):
+    cost = 0
+    for action in combination:
+        cost = cost + action["cost"]
+    return cost
+
+
+def calculate_profit(combination):
+    profit = 0
+    for action in combination:
+        profit = profit + (action["cost"] * (1 + (action["profit"] / 100)))
+    return profit
+
+
+def bruteforce():
+    tabLen = len(actions)
+    bestProfit = 0
+    bestCombination = ()
+    for r in range(tabLen - 1):
+        for combination in combinations(actions, r):
+            cost = calculate_cost(combination)
+            profit = calculate_profit(combination)
+            if cost <= MAX_EXPENSE and profit > bestProfit:
+                bestProfit = profit
+                bestCombination = combination
+    return bestCombination
 
 
 if __name__ == "__main__":
     readFile()
-    print(actions)
+    best = bruteforce()
+    cost = calculate_cost(best)
+    profit = calculate_profit(best)
+    print(f"le cout est de {cost}, le profit de {profit}")
+    print(best)
