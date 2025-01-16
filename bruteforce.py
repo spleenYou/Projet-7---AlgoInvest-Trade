db25b9d47ce71d93d300988184eff34a9a2909a9
Anthony YOU <spleen85@proton.me>
2025-01-16 13:06:33 +0100

    Try something : count the value after 2 year at start

--- bruteforce.py
+++ bruteforce.py
@@ -15,7 +15,8 @@ def readFile():
             tab.append({
                 "name": data[0],
                 "cost": int(data[1]),
-                "profit": int(data[2].replace("%", ""))
+                "value_after": int(data[1]) * (1 + int(data[2].replace("%", "")) / 100)
+                # "profit": int(data[2].replace("%", ""))
             })
     return tab
 
@@ -24,8 +25,8 @@ def sum_total_cost(combination):
     return sum(action["cost"] for action in combination)
 
 
-def sum_total_profit(combination):
-    return sum(action["cost"] * (1 + (action["profit"] / 100)) for action in combination)
+def sum_total_value_after_2_year(combination):
+    return sum(action["value_after"] for action in combination)
 
 
 def bruteforce(actions):
@@ -36,8 +37,8 @@ def bruteforce(actions):
         for combination in combinations(actions, r):
             cost = sum_total_cost(combination)
             if cost <= MAX_EXPENSE:
-                profitCost = sum_total_profit(combination)
-                profit = profitCost - cost
+                value = sum_total_value_after_2_year(combination)
+                profit = value - cost
                 if profit > bestProfit:
                     bestProfit = profit
                     bestCombination = combination
@@ -50,9 +51,9 @@ if __name__ == "__main__":
     best = bruteforce(actions)
     print(f"Temps d'éxecution : {round(time.time() - start, 2)}")
     cost = sum_total_cost(best)
-    profit = sum_total_profit(best)
+    profit = sum_total_value_after_2_year(best)
     for action in best:
-        print(f"{action['name']} pour un prix de {action['cost']}€ avec un profit de {action['profit']}%")
+        print(f"{action['name']} pour un prix de {action['cost']}€ avec un profit de {action['value_after']}%")
     print(f"le cout total est de {cost}€"
           f"pour un profit de {round(profit, 2)}€"
           f"soit {round((profit/cost) - 1, 2) * 100}%"
