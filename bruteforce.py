66b66716e5560445c146b32e3e7d81bb3f67508d
Anthony YOU <spleen85@proton.me>
2025-01-16 13:15:53 +0100

    Rename variables and add space in the last print

--- bruteforce.py
+++ bruteforce.py
@@ -2,8 +2,6 @@ import csv
 from itertools import combinations
 import time
 
-MAX_EXPENSE = 500
-
 
 def readFile():
     tab = []
@@ -29,32 +27,33 @@ def sum_total_value_after_2_year(combination):
     return sum(action["value_after"] for action in combination)
 
 
-def bruteforce(actions):
+def bruteforce(actions, MAX_EXPENSE):
     tabLen = len(actions)
-    bestProfit = 0
-    bestCombination = ()
+    best_profit = 0
+    best_combination = ()
     for r in range(1, tabLen):
         for combination in combinations(actions, r):
             cost = sum_total_cost(combination)
             if cost <= MAX_EXPENSE:
                 value = sum_total_value_after_2_year(combination)
                 profit = value - cost
-                if profit > bestProfit:
-                    bestProfit = profit
-                    bestCombination = combination
-    return bestCombination
+                if profit > best_profit:
+                    best_profit = profit
+                    best_combination = combination
+    return best_combination
 
 
 if __name__ == "__main__":
+    MAX_EXPENSE = 500
     actions = readFile()
     start = time.time()
-    best = bruteforce(actions)
+    best = bruteforce(actions, MAX_EXPENSE)
     print(f"Temps d'éxecution : {round(time.time() - start, 2)}")
     cost = sum_total_cost(best)
     profit = sum_total_value_after_2_year(best)
     for action in best:
         print(f"{action['name']} pour un prix de {action['cost']}€ avec un profit de {action['value_after']}%")
-    print(f"le cout total est de {cost}€"
-          f"pour un profit de {round(profit, 2)}€"
-          f"soit {round((profit/cost) - 1, 2) * 100}%"
+    print(f"le cout total est de {cost}€ "
+          f"pour un profit de {round(profit, 2)}€ "
+          f"soit {round((profit/cost) - 1, 2) * 100}% "
           f"soit {round(profit - cost, 2)}€")
