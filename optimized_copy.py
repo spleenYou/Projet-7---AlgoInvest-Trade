aee6a7f177ebe49f15a65a3b408d850363714ac7
Anthony YOU <spleen85@proton.me>
2025-01-27 19:59:57 +0100

    Change def somme() to be more effective, change some variable's names and optimize

--- optimized_copy.py
+++ optimized_copy.py
@@ -61,29 +61,27 @@ def execution_information(func):
     return wrapper
 
 
-def somme(item, tab, x=None):
-    if x is None:
-        x = {item: 0}
-    return float(sum(x[item] for x in tab) + x[item])
+def somme(item, tab):
+    return float(sum(x[item] for x in tab if x["name"] is not None))
 
 
-def sac_a_dos(poids_max, tab, result):
-    if len(tab) == 0:
+def sac_a_dos(poids_max, actions, result):
+    n = len(actions) - 1
+    if n == 0:
         return result
-    x = tab.pop()
-    n = len(tab)
-    rent1 = somme("profit", result[0][n + 1], x)
+    tab = result[0][n + 1].copy()
+    tab.append(actions.pop())
+    rent1 = somme("profit", tab)
     rent2 = somme("profit", result[1][n + 1])
     if rent1 > rent2:
-        poids1 = somme("cost", result[0][n + 1], x)
-        if poids1 <= poids_max:
-            result[1][n] = result[0][n + 1].copy()
-            result[1][n].append(x)
+        cost = somme("cost", tab)
+        if cost <= poids_max:
+            result[1][n] = tab
         else:
             result[1][n] = result[1][n + 1].copy()
     else:
         result[1][n] = result[1][n + 1]
-    return sac_a_dos(poids_max, tab, result)
+    return sac_a_dos(poids_max, actions, result)
 
 
 @execution_information
@@ -101,11 +99,12 @@ def optimized(MAX_EXPENSE, actions):
     # Find the number of actions
     n = len(actions)
     actions = sorted(actions, key=lambda x: x["cost"]/x["profit"], reverse=True)
-    tab = [[[{"name": None, "cost": 0, "profit": 0}]] * (n + 1)] * 2
-    for i in range(0, n):
+    tab = [[[{"name": None, "cost": 0, "profit": 0}] for _ in range(n + 1)] for _ in range(2)]
+    tab[1][0][0]["cost"] = 1
+    while tab[0] != tab[1]:
         tab[0] = tab[1].copy()
         tab = sac_a_dos(MAX_EXPENSE, actions.copy(), tab)
-    return tab[1][0]
+    return tab[1][1]
 
 
 if __name__ == "__main__":
@@ -122,6 +121,6 @@ if __name__ == "__main__":
     # Show the total
     cost = somme("cost", best_combination)
     profit = somme("profit", best_combination)
-    print(f"le cout total est de {cost}€ "
+    print(f"le cout total est de {cost:.2f}€ "
           f"pour un profit de {profit-cost:.2f}€ "
           f"soit {((profit/cost) - 1) * 100:.2f}%")
